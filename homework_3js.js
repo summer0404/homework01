07bd1fde36a132770ada2cf62563937fb095d916
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-03-01 15:50:30 +0700

    Update homework_3js.js

--- homework_3js.js
+++ homework_3js.js
@@ -1,4 +1,8 @@
 function getInput(){
     var input=document.getElementById("txtin").value;
-    alert("Your resquest of \"" +input + "\" is being processed");
+    if (input == "" || input == null){
+        alert("Please enter a valid resquest.");
+        return;
+    }
+    alert("Your resquest of \"" +input + "\" is being processed.");
 }
